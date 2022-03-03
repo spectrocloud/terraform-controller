e7cd4e77af8a5f555cba3f2b171af1d82444101e
LochanRn <rajanaga_ra@srmuniv.edu.in>
2022-03-03 22:47:16 +0530

    updated the sync period

--- controllers/configuration_controller.go
+++ controllers/configuration_controller.go
@@ -213,7 +213,8 @@ func (r *ConfigurationReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	if err != nil {
 		klog.ErrorS(err, "Terraform apply failed")
 		if updateErr := meta.updateApplyStatus(ctx, r.Client, state, err.Error()); updateErr != nil {
-			return ctrl.Result{}, updateErr
+			// fmt.Println("RequeueAfter: 3 * time.Minute")
+			return ctrl.Result{RequeueAfter: 3 * time.Minute}, updateErr
 		}
 	}
 
@@ -740,7 +741,7 @@ func (meta *TFConfigurationMeta) assembleTerraformJob(executionType TerraformExe
 						Command: []string{
 							"bash",
 							"-c",
-							fmt.Sprintf("terraform init --plugin-dir /providers/plugins && /providers/exec.sh %s -lock=false -auto-approve", executionType),
+							fmt.Sprintf("terraform init --plugin-dir /providers/plugins && /providers/exec.sh %s", executionType),
 						},
 
 						VolumeMounts: append([]v1.VolumeMount{
